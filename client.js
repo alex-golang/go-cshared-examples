0e9b38bbed3f3614ee5688e8bc430dedfd76d6db
Alex Davis <alex2006hw@gmail.com>
2021-06-02 06:37:50 +0000

    patched-for-node15

--- client.js
+++ client.js
@@ -1,10 +1,68 @@
-var ref = require("ref");
-var ffi = require("ffi");
-var Struct = require("ref-struct")
-var ArrayType = require("ref-array")
+var ref = require("ref-napi");
+var ffi = require("ffi-napi");
+var weak = require('weak-napi');
+// var Struct = require("ref-struct")
+var Struct = require('ref-struct-di')(ref)
+// var ArrayType = require("ref-array")
+var ArrayType = require('ref-array-di')(ref)
+var Union = require('ref-union-di')(ref)
+
 
 var longlong = ref.types.longlong;
 var LongArray = ArrayType(longlong);
+
+////// start union map
+// a couple typedefs
+var int = ref.types.int
+var float = ref.types.float
+var string = ref.types.CString
+
+// define a Union type with 3 data fields
+var u_tag = new Union({
+    ival: int
+  , fval: float
+  , sval: string
+})
+
+// the size of the union matches the largest data type in the union type
+console.log('1.client.js union tag:',u_tag.size , string.size ,u_tag.size === string.size)
+
+// and you can create new instances of the union type
+var tag = new u_tag
+tag.ival = 5
+
+console.log('2.client.js union tag.ival:',tag.ival)
+
+////// end union map
+////// start weak referencing
+// we are going to "monitor" this Object and invoke "cleanup"
+// before the object is garbage collected
+var obj = {
+    a: true
+  , foo: 'bar'
+}
+
+// Here's where we set up the weak reference
+var ref = weak(obj, function () {
+  // `this` inside the callback is the EventEmitter.
+  console.log('1.client.js weak "obj" has been garbage collected!')
+})
+
+console.log('2.client.js weak ref.foo:',typeof ref.foo)
+// While `obj` is alive, `ref` proxies everything to it, so:
+console.log('3.client.js weak ref.a:',ref.a, obj.a, ref.a   === obj.a)
+console.log('4.client.js weak ref.foo:',ref.foo, obj.foo, ref.foo === obj.foo)
+
+// Clear out any references to the object, so that it will be GC'd at some point...
+obj = null
+
+//
+//// Time passes, and the garbage collector is run
+//
+
+// `callback()` above is called, and `ref` now acts like an empty object.
+console.log('5.client.js weak ref.foo(undefined):',typeof ref.foo)
+/////// end weak referencing
 
 // define object GoSlice to map to:
 // C type struct { void *data; GoInt len; GoInt cap; }
@@ -29,12 +87,6 @@ var awesome = ffi.Library("./awesome.so", {
   Log: ["longlong", [GoString]]
 });
 
-// call Add
-console.log("awesome.Add(12, 99) = ", awesome.Add(12, 99));
-
-// call Cosine
-console.log("awesome.Cosine(1) = ", awesome.Cosine(1));
-
 // call Sort
 nums = LongArray([12,54,0,423,9]);
 var slice = new GoSlice();
@@ -42,10 +94,17 @@ slice["data"] = nums;
 slice["len"] = 5;
 slice["cap"] = 5;
 awesome.Sort(slice);
-console.log("awesome.Sort([12,54,9,423,9] = ", nums.toArray());
+console.log("1.client.js awesome.Sort([12,54,9,423,9] = ", nums.toArray());
+
+// call Add
+console.log("2.client.js awesome.Add(12, 99) = ", awesome.Add(12, 99));
+
+// call Cosine
+console.log("3.client.js awesome.Cosine(1) = ", awesome.Cosine(1));
 
 // call Log
 str = new GoString();
-str["p"] = "Hello Node!";
-str["n"] = 11;
+str["p"] = "4.client.js awesome.Log = Hello Node!";
+str["n"] = str["p"].length;
 awesome.Log(str);
+//////
